d4eb1b602a7d635615bdef60cbed80fd301e3115
zloy27 <151875323+zloy27@users.noreply.github.com>
2024-03-03 15:52:51 +0300

    79 урок

---
+++ 79 урок темплейт/79 урок/js/main.js
@@ -0,0 +1,7 @@
+const age = 10 
+if(age>=18){
+    console.log('Вы совершенно летний')
+}else{
+    console.log('вы несворшенно летний')
+}
+age>=18? console.log('Вы совершенно летний'): console.log('вы несворшенно летний')
