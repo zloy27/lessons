46d4bb7de12d5d86d8e750a6a2a967e3aa46ef57
zloy27 <151875323+zloy27@users.noreply.github.com>
2024-03-10 15:06:05 +0300

    js 80 и 81 урок

---
+++ 80 урок/js/main.js
@@ -0,0 +1,17 @@
+function reverse() {
+for(let n = 0; n<=10;n++) {
+    console.log(l)
+}}
+for( i = 0; i<=12; i= i+2 ) {
+    console.log(i)
+}
+let l = 0
+while(l<10) {
+    l = l+1
+    console.log(l)
+}
+let k = 11
+    while(k>1) {
+        k = k-1
+        console.log(k)
+    }
