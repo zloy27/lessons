6fd837eb2ca1326f8412668b0a19aa5fbd1dae1e
zloy27 <151875323+zloy27@users.noreply.github.com>
2024-07-27 22:51:58 +0300

    Delete 79 урок темплейт directory

--- 79 урок темплейт/79 урок/js/main.js
+++
@@ -1,7 +0,0 @@
-const age = 10 
-if(age>=18){
-    console.log('Вы совершенно летний')
-}else{
-    console.log('вы несворшенно летний')
-}
-age>=18? console.log('Вы совершенно летний'): console.log('вы несворшенно летний')
